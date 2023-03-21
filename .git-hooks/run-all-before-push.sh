3618b6f113c50ef3bb79b26bef58b73a8d67fdee
Hamid Molareza <kitm.hamid@gmail.com>
2023-03-21 12:19:58 +0330

    build: update run-all-before-push.sh

--- .git-hooks/run-all-before-push.sh
+++ .git-hooks/run-all-before-push.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 npm run all
+
+build_dirs_status=$(git status --short dist/ lib/)
+result_code=$?
+if [ $result_code != 0 ]; then
+  echo "Get git status failed."
+  exit $?
+fi
+
+if [ "$build_dirs_status" != "" ]; then
+  echo "There are uncommitted files in 'dist' and 'lib' dirs. Please commit them before pushing."
+  exit 1
+fi
