89d34c17c69798643d7c55e58a516d1964a4363f
Hamid Molareza <kitm.hamid@gmail.com>
2023-03-21 12:32:27 +0330

    build: improve messages in run-all-before-push.sh

--- .git-hooks/run-all-before-push.sh
+++ .git-hooks/run-all-before-push.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+script_name="$(basename -- $0)"
+
+say_message() {
+  local message="$1"
+  echo "$script_name: $message"
+}
+
+say_message "npm run all..."
 npm run all
 
 build_dirs_status=$(git status --short dist/ lib/)
 result_code=$?
 if [ $result_code != 0 ]; then
-  echo "Get git status failed."
+  say_message "Error! Get git status failed."
   exit $?
 fi
 
 if [ "$build_dirs_status" != "" ]; then
-  echo "There are uncommitted files in 'dist' and 'lib' dirs. Please commit them before pushing."
+  say_message "Error! There are uncommitted files in 'dist' and 'lib' dirs. Please commit them before pushing."
   exit 1
 fi
