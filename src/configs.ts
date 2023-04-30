939da79b63a6b91a840f8e3c75c539f204564bf5
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-30 22:42:59 +0330

    refactor: reorder DEFAULT_INPUTS

--- src/configs.ts
+++ src/configs.ts
@@ -5,18 +5,17 @@ export const SEMANTIC_VERSION_REGEX =
     /(0|[1-9]\d*)(\.\d+){0,2}(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/;
 
 export const DEFAULT_INPUTS: IInputs = {
-    isTestMode: false,
-    gitEmail: "github-action@github.com",
-    gitUsername: "Github Action",
     inputVersion: "",
-    versionRegex: SEMANTIC_VERSION_REGEX,
     ignoreSameVersionError: false,
     ignoreLessVersionError: false,
+    createPrForBranchName: "",
     generateChangelog: "auto",
     skipReleaseFile: true,
     releaseDirectory: ".",
     releaseFileName: "release",
-    createPrForBranchName: "",
+    isTestMode: false,
+    gitEmail: "github-action@github.com",
+    gitUsername: "Github Action",
+    versionRegex: SEMANTIC_VERSION_REGEX,
     changelogHeaderRegex: DEFAULT_CHANGELOG_HEADER_REGEX,
-    currentVersion: "",
 };
