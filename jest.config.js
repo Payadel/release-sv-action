83b86afc8874bca5464b60a66b70d0ea1c59cfd1
Hamid Molareza <kitm.hamid@gmail.com>
2023-04-30 20:11:16 +0330

    test: fix `collectCoverageFrom` in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -16,7 +16,7 @@ exports.default = {
   // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ["src/*.{js,jsx,ts}", "!src/main.ts"],
+  collectCoverageFrom: ["src/**/*.{js,jsx,ts}", "!src/main.ts"],
   // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
   // An array of regexp pattern strings used to skip coverage collection
